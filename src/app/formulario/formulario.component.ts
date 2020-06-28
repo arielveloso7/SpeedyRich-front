2942cbba0058271648e41d56de586e630bb2389c
arielveloso <el_veloso@hotmail.com>
2020-06-28 21:14:17 +0200

    actualizacion form

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -17,19 +17,19 @@ export class FormularioComponent implements OnInit {
   constructor(private appService: AppService) {
 
     this.formulario = new FormGroup({
-      nombre: new FormControl('', [Validators.required]),
-      apellidos: new FormControl('', [Validators.required]),
-      dni: new FormControl('', [Validators.required]),
-      clave: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [Validators.required, , Validators.minLength(3)]),
+      apellidos: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      dni: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]),
+      clave: new FormControl('', [Validators.required, Validators.minLength(3)]),
       fecha_nacimiento: new FormControl('', [Validators.required]),
-      direccion: new FormControl('', [Validators.required]),
-      telefono: new FormControl('', [Validators.required]),
+      direccion: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      telefono: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]),
       email: new FormControl('', [
         Validators.required,
         Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
       ]),
       ingresos: new FormControl('', [Validators.required]),
-      banco: new FormControl('', [Validators.required]),
+      banco: new FormControl('ES', [Validators.required, Validators.minLength(24), Validators.maxLength(24)]),
       image: new FormControl('', [Validators.required])
     })
   }
